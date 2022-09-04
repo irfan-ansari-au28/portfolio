1dd7de1622ee0594a9210dce725e935fde829ed3
apple <irfan398649@gmail.com>
2022-09-04 23:48:40 +0530

    add head data for SEO

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import FeaturedPosts from "../components/home-page/featured-post";
 import Hero from "../components/home-page/hero";
@@ -6,6 +7,10 @@ import { getFeaturedPosts } from "../lib/posts-util";
 const HomePage = ({ posts }) => {
   return (
     <>
+      <Head>
+        <title>phoreka's Portfolio</title>
+        <meta name="description" content="My personal website" />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </>
