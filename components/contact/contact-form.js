2dcc61a5b0da9f56da9bd76860e10c31e3d43b93
apple <irfan398649@gmail.com>
2022-09-04 22:28:34 +0530

    add contact api route, setup mongodb

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ContactForm = () => {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+
+  function sendMessageHandler(event) {
+    event.preventDefault();
+
+    // optional : client side validation
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        name: enteredName,
+        message: enteredMessage,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
   return (
     <section>
       <h1>How I can help you?</h1>
-      <form>
+      <form onSubmit={sendMessageHandler}>
         <div>
           <label htmlFor="email"> Your Email</label>
-          <input type="email" id="email" required />
+          <input
+            type="email"
+            id="email"
+            required
+            value={enteredEmail}
+            onChange={(event) => setEnteredEmail(event.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="name"> Your Name</label>
-          <input type="text" id="name" required />
+          <input
+            type="text"
+            id="name"
+            required
+            value={enteredName}
+            onChange={(event) => setEnteredName(event.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="message"> Your Message</label>
-          <input id="message" rows="5" />
+          <input
+            id="message"
+            rows="5"
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          />
         </div>
         <div>
           <button>Send Message</button>
