9293fea4f09f6f5a1238efe213e6352c03ad6e0e
apple <irfan398649@gmail.com>
2022-09-06 23:27:31 +0530

    add few skills svg

--- components/slider.js/Slider.js
+++ components/slider.js/Slider.js
@@ -1,35 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import CssLogo from "../../public/icons/css-logo";
 import HtmlLogo from "../../public/icons/html-logo";
 import NextJSLogo from "../../public/icons/next-js-logo";
 import Python from "../../public/icons/python-logo";
 import classes from "./slider.module.css";
 
-export const Rectangle = () => {
-  return (
-    <div
-      style={{
-        display: "inline-block",
-        height: "40px",
-        width: "100px",
-        border: "2px solid black",
-        borderRadius: "5px",
-        margin: "10px",
-      }}
-    >
-      Rectangle
-    </div>
-  );
-};
+import {
+  FaAngular,
+  FaCss3,
+  FaGithub,
+  FaHtml5,
+  FaJs,
+  FaReact,
+} from "react-icons/fa";
 
 const Slider = () => {
   return (
     <div>
       <div className={classes.sliderContainer}>
         <div className={`${classes.sliderContentWrapper} ${classes.fast}`}>
-          <Python />
-          <HtmlLogo />
-          <CssLogo />
+          <div className="mx-4">
+            <FaHtml5
+              size="4rem"
+              // color={`${true ? "rgb(0,0,0,.8)" : "rgb(0,0,0,.2)"}`}
+            />
+          </div>
+          <div className="mx-4">
+            <FaCss3
+              size="4rem"
+              // color={`${true ? "rgb(0,0,0,.8)" : "rgb(0,0,0,.2)"}`}
+            />
+          </div>
+          <div className="mx-4">
+            <FaJs
+              size="4rem"
+              // color={`${true ? "rgb(0,0,0,.8)" : "rgb(0,0,0,.2)"}`}
+            />
+          </div>
+          <div className="mx-4">
+            <FaAngular
+              size="4rem"
+              // color={`${true ? "rgb(0,0,0,.8)" : "rgb(0,0,0,.2)"}`}
+            />
+          </div>
+          <div className="mx-4">
+            <FaReact
+              size="4rem"
+              // color={`${true ? "rgb(0,0,0,.8)" : "rgb(0,0,0,.2)"}`}
+            />
+          </div>
+          <div className="mx-4">
+            <FaGithub
+              size="4rem"
+              // color={`${true ? "rgb(0,0,0,.8)" : "rgb(0,0,0,.2)"}`}
+            />
+          </div>
           <NextJSLogo />
         </div>
       </div>
--- public/icons/next-js-logo.js
+++ public/icons/next-js-logo.js
@@ -5,8 +5,8 @@ const NextJSLogo = () => {
     <div>
       <svg
         fill="none"
-        width="96px"
-        height="96px"
+        width="4rem"
+        height="4rem"
         viewBox="0 0 394 80"
         xmlns="http://www.w3.org/2000/svg"
       >
