c50af456fa40140b426b356fb12b5c3961e55d87
apple <irfan398649@gmail.com>
2022-09-12 13:31:33 +0530

    style portfolio-card icon

--- components/link/external-link.js
+++ components/link/external-link.js
@@ -2,12 +2,7 @@ import React from "react";
 
 const ExternalLink = ({ href, children }) => {
   return (
-    <a
-      href={href}
-      className="font-semibold text-primary transition duration-300 ease-in-out"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <a href={href} className=" " target="_blank" rel="noopener noreferrer">
       {children}
     </a>
   );
--- components/portfolio-page/portfolio-card.js
+++ components/portfolio-page/portfolio-card.js
@@ -10,8 +10,12 @@ const PortfolioCard = () => {
           <div className="max-w-7xl mx-auto">
             <div className="relative group">
               <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
-              <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
-                <FiCode size={"2rem"} />
+              <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-center justify-start space-x-6">
+                <div className="bg-lime-100 w-12 h-12 rounded-full flex items-center justify-center">
+                  <div>
+                    <FiCode size={"2rem"} />
+                  </div>
+                </div>
                 <div className="space-y-2">
                   <p className="text-slate-800">Url Shortener</p>
                 </div>
